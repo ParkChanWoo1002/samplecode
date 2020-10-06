52dee1cb50226d1a29c5b39bc2e039f470183df1
ParkChanWoo1002 <63335697+ParkChanWoo1002@users.noreply.github.com>
2020-10-06 19:30:12 +0900

    static 변수
    객체의 개수를 알아보는 프로그램

---
+++ 5주차 static.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream> //20173504박찬우
+using namespace std;
+
+int generateID()
+{
+	int a = 0;
+	return ++a;
+}
+
+int main()
+{
+	cout << "s_id(1)" << generateID() << endl;
+	cout << "s_id(2)" << generateID() << endl;
+	cout << "s_id(3)" << generateID() << endl;
+	return 0;
+}
+// main함수에서 generateID()함수를 호출하게되고 s_id라는 변수를 선언 0으로 초기화하고  s_id값을 증감하고 반환해줘서
+//1을 출력하게된다. s_id는 지역변수 이기때문에 벗어나면 값과 할당된 메모리를 삭제하기때문에 s_id라는 변수는 없어진다.
+// 그리고나서 generateID()로 다시 호출하는데 앞에 선언했던 s_id라는 변수는 사라졌기때문에 새로운 s_id라는 변수를
+//선언하고 증감함으로써 또 1을 출력하게된다.
---
+++ 5주차 static2.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+class Something
+{
+    static int m_value; //정적 멤버 변수 선언
+public:
+    Something(); // 생성자 선언
+    int result(); // 멤버 함수 선언
+};
+int Something::m_value = 0; // 멤버 변수 초기화
+Something::Something() // 생성자 정의
+{
+   m_value++; // 생성자가 호출되면 m_value값을 증감시킴
+}
+int Something::result() // 멤버함수 정의
+{
+    return m_value; // m_value값을 반환 하는 멤버 함수
+}
+int main()
+{
+    cout << "객체의 개수를 알아보는 프로그램" << endl;
+    Something year; // 객체를 생성함으로써 생성자가 호출됨
+    cout << "현재 객체의 수 : " << year.result() << "개" << endl; // 생성자가 호출되면서 value값이 증감되고 result를 호출하면서 1을 반환
+    Something month; // 객체를 생성함으로써 생성자가 다시 호출됨 m_value값은 static 변수이기때문에 값이 남아있어 2로 증감
+    cout << "현재 객체의 수 : " << month.result() << "개" << endl; // result를 호출하면서 2를 반환함
+    Something day; // 객체를 생성함으로써 생성자가 또다시호출되고 m_value값은 3으로 증감
+    cout << "현재 객체의 수 : " << day.result() << "개" << endl; // result를 호출하면서 3을 반환함
+    return 0;
+}
+
